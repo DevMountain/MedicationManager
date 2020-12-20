183386f9e79408bbd51a0323b8cad054ce24adb0
aaronmartinez <aaroncmartinez@ldschurch.org>
2020-12-20 00:03:50 -0700

    Add xcode project

---
+++ MedicationManager/MedicationManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedicationManager
+//
+//  Created by Aaron Martinez on 12/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
